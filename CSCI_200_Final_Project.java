c6dda4d119e11dc14c36b3c7886be64b4eb18b1a
Boi12232 <shaminemailthing@gmail.com>
2025-11-21 14:22:48 -0600

    Update CSCI_200_Final_Project.java

--- CSCI_200_Final_Project.java
+++ CSCI_200_Final_Project.java
@@ -1,6 +1,6 @@
 public class CSCI_200_Final_Project
 {
-  public static void main(String[] args)
+  public static void main(String[] args) throw IOException
   {
     RPGGame game = new RPGGame();
     game.startGame(); //Start the game loop
