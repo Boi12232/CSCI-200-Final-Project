2edfe348e1122d783d14ac1c8061593842b6f72d
Boi12232 <shaminemailthing@gmail.com>
2025-12-03 15:45:12 -0600

    Update CSCI_200_Final_Project.java

--- CSCI_200_Final_Project.java
+++ CSCI_200_Final_Project.java
@@ -1,8 +1,13 @@
+import java.util.*;
+import java.io.*;
+
 public class CSCI_200_Final_Project
 {
-  public static void main(String[] args) throw IOException
-  {
-    RPGGame game = new RPGGame();
+  public static void main(String[] args) throws IOException, InterruptedException
+  { 
+    RPGGame game = new RPGGame("locations.txt", "enemies.txt", "items.txt");
+    //Uncomment next line to see the raw graph and region structure
+    //game.debugPrintWorld();
     game.startGame(); //Start the game loop
   }
 }
