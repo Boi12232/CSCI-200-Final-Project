d9a4b70e431f83f6fac968a48bcdd5e8b71346dc
Mai-Ke-Ke <lormaike@gmail.com>
2025-12-06 15:39:52 -0600

    Update Location.java

--- Location.java
+++ Location.java
@@ -1,126 +1,66 @@
 import java.util.*;
 
-public class Location{
-  //Attributes
+public class Location 
+{
   private String name;
   private int encounterChance;
   private int fleeInfluence;
   private String description;
-  protected List<Enemy> enemies = new ArrayList<>();
-  private List<String> items = new ArrayList<>();
+  private Biome biome;
   
-  /**
-   * Constructor for the Location
-   * @param name represents the name of the location
-   * @param encounterChance represents an int for the chances of encountering an enemy
-   * @param fleeInfluence is used to find the possibility for fleeing from this particular enemy
-   * @param description represents the description of the enemy
-   */
-  public Location(String name, int encounterChance, int fleeInfluence, String description) 
+  public Location(String name, int encounterChance, int fleeInfluence, String description, Biome biome) 
   {
     this.name = name;
     this.encounterChance = encounterChance;
     this.fleeInfluence = fleeInfluence;
     this.description = description;
+    this.biome = biome;
   }
   
-  /**
+   /**
    * Constructor for creating a shelter location
    * @param name represents the shelter name
    */
-  public Location(String name){
+  public Location(String name, Biome currentBiome){
     this.name = name;
     this.encounterChance = 0;
     this.fleeInfluence = 0;
     this.description = "Shelter";
+    this.biome = currentBiome;
     
     
   }
   
-  /**
-   * Name getter
-   * @return the name
-   */
-  public String getName() 
-  { 
-    return name; 
-  }
-  /**
-   * Encounter chance getter
-   * @return encounter chance of location
-   */
-  public int getEncounterChance() 
-  { 
-    return encounterChance; 
-  }
-  /**
-   * Flee influence getter
-   * @return the flee influence of location
-   */
-  public int getFleeInfluence() 
-  { 
-    return fleeInfluence; 
-  }
-  /**
-   * Description getter
-   * @return the location's description
-   */
-  public String getDescription() 
-  { 
-    return description; 
-  }
-  /**
-   * Enemies getter
-   * @return a list of the location's enemies
-   */
-  public List<Enemy> getEnemies() 
-  { 
-    return enemies; 
-  }
-  /**
-   * Items getter
-   * @return a list of the location's items
-   */
-  public List<String> getItems() 
-  { 
-    return items; 
+  public String getName()
+  {
+    return name;
   }
   
-  /**
-   * Equals method
-   */
+  public Biome getBiome() 
+  { 
+    return biome; 
+  }
+  
+  public int getEncounterChance() {
+    return encounterChance;
+  }
+  
   @Override
   public boolean equals(Object o) 
   {
     if (!(o instanceof Location)) return false;
     return name.equals(((Location)o).name);
   }
-  /**
-   * Hash code
-   */
+  
   @Override
   public int hashCode() 
-  {
-    return name.hashCode();
+  { 
+    return name.hashCode(); 
   }
-  /**
-   * To string
-   */
+  
   @Override
   public String toString() 
-  {
-    return name;
+  { 
+    return name; 
   }
-  
-//  @Override
-//  public int compareTo(T otherLocation){
-//    Location other = (Location)otherLocation;
-//    if(this.name ==  other.getName()){
-//    return 0;
-//    }
-//    else{
-//    return 1;
-//    }
-//  }
-  
 }
