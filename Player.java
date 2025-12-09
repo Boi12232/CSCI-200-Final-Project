cc22c7692e1f801ba2f6847540a9b3a98a64f183
Mai-Ke-Ke <lormaike@gmail.com>
2025-12-08 22:07:43 -0600

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,14 +1,30 @@
+/**
+ * Shamin 
+ * Player class: creates and contains the methods of the Player Object. The player data.
+ * 12/7/2025
+ */
 import java.util.*;
 
 public class Player 
 {
+  /**
+   * Attributes
+   */
   private String name;
   private int maxHP;
   private int currentHP;
   private int attack;
   private int defense;
-  private List<Item> inventory;
+  private Inventory inventory;
   
+  
+  /**
+   * Constructor 
+   * @param name represents the user's name;
+   * @param hp represents the starting health of the user
+   * @param attack represents the starting attack damage of the user
+   * @param defense represents the defense value of the user
+   */
   public Player(String name, int hp, int attack, int defense) 
   {
     this.name = name;
@@ -16,45 +32,74 @@ public class Player
     this.currentHP = hp;
     this.attack = attack;
     this.defense = defense;
-    this.inventory = new ArrayList<>();
+    this.inventory = new Inventory();
   }
   
+  /**
+   * This method returns the user's name 
+   * @return String of the user's name
+   */
   public String getName() 
   { 
     return name; 
   }
   
+  /**
+   * This method returns the user's name 
+   * @return String of the user's name
+   */
   public int getHP() 
   { 
     return currentHP; 
     
   }
   
+  /**
+   * This method returns the user's attack damage 
+   * @return int of the user's attack damage
+   */
   public int getAttack() 
   { 
     return attack;
   }
   
+  /**
+   * This method returns the user's defense  
+   * @return int of the user's defense
+   */
   public int getDefense() 
   { 
     return defense; 
   }
   
+  /**
+   * This method increases the user's HP 
+   * @param val represents the amount the user's health will increase by
+   */
   public void addHP(int val) 
   {
     currentHP += val;
     if (currentHP > maxHP) currentHP = maxHP;
   }
   
+    /**
+   * This method increases the user's attack damage
+   * @param val represents the amount the user's attack will increase by
+   */
   public void addATK(int val)
   {
     attack += val;
   }
   
+    /**
+   * This method increases the user's defense
+   * @param val represents the amount the user's defense will increase by
+   */
   public void addDEF(int val)
   {
     defense += val;
   }
+  
   
   public void takeDamage(int dmg) 
   {
@@ -62,18 +107,22 @@ public class Player
     if (currentHP < 0) currentHP = 0;
   }
   
+  /**
+   * This method returns a boolean on if the user is alive or not
+   * @return boolean true if the user's health is greater than 0; else false.
+   */
   public boolean isAlive() 
   {
     return currentHP > 0;
   }
   
-  public void addItem(Item item) 
-  {
-    inventory.add(item);
-  }
-  
-  public List<Item> getInventory() 
+  /**
+   *This method gets the player's inventory
+   * @return Inventory 
+   */
+  public Inventory getInventory() 
   {
     return inventory;
   }
+  
 }
